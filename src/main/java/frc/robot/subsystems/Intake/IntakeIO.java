f0d6f0e84b2119c076413345baf8d8b380304d6a
Avi Sharma <64670742+Av1Sharma@users.noreply.github.com>
2026-01-22 18:24:45 -0500

    indexer done + gradle error fixed

--- src/main/java/frc/robot/subsystems/Intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeIO.java
@@ -1,62 +1,51 @@
 package frc.robot.subsystems.Intake;
 
-import org.littletonrobotics.junction.AutoLog;
+import static edu.wpi.first.units.Units.*;
 
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Temperature;
 import edu.wpi.first.units.measure.Voltage;
-
-import static edu.wpi.first.units.Units.*;
+import org.littletonrobotics.junction.AutoLog;
 
 public interface IntakeIO {
-	@AutoLog
-	public static class IntakeIOInputs {
-		public Angle wheelPosition = Radians.of(0);
-		public AngularVelocity wheelVelocity = RadiansPerSecond.of(0);
-		public AngularVelocity wheelDesiredVelocity = RadiansPerSecond.of(0);
-		public AngularVelocity wheelVelocitySetpoint = RadiansPerSecond.of(0);
-		public Voltage wheelAppliedVolts = Volts.of(0);
-		public Current wheelSupplyCurrent = Amps.of(0);
-		public Temperature wheelMotorTemperature = Celsius.of(0);
+  @AutoLog
+  public static class IntakeIOInputs {
+    public Angle wheelPosition = Radians.of(0);
+    public AngularVelocity wheelVelocity = RadiansPerSecond.of(0);
+    public AngularVelocity wheelDesiredVelocity = RadiansPerSecond.of(0);
+    public AngularVelocity wheelVelocitySetpoint = RadiansPerSecond.of(0);
+    public Voltage wheelAppliedVolts = Volts.of(0);
+    public Current wheelSupplyCurrent = Amps.of(0);
+    public Temperature wheelMotorTemperature = Celsius.of(0);
 
-		public Angle pivotDesiredPositionSetpoint = Radians.of(0);
-		public Angle pivotDesiredPosition = Radians.of(0);
-		public AngularVelocity pivotVelocity = RadiansPerSecond.of(0);
-		public Angle pivotPosition = Radians.of(0);
-		public Voltage pivotAppliedVolts = Volts.of(0);
-		public Current pivotSupplyCurrent = Amps.of(0);
-		public Temperature pivotTemperature = Celsius.of(0);
-	}
+    public Angle pivotDesiredPositionSetpoint = Radians.of(0);
+    public Angle pivotDesiredPosition = Radians.of(0);
+    public AngularVelocity pivotVelocity = RadiansPerSecond.of(0);
+    public Angle pivotPosition = Radians.of(0);
+    public Voltage pivotAppliedVolts = Volts.of(0);
+    public Current pivotSupplyCurrent = Amps.of(0);
+    public Temperature pivotTemperature = Celsius.of(0);
+  }
 
-	public default void updateInputs(IntakeIOInputs inputs) {
-	}
+  public default void updateInputs(IntakeIOInputs inputs) {}
 
-	public default void stopWheel() {
-	}
+  public default void stopWheel() {}
 
-	public default void stopPivot() {
-	}
+  public default void stopPivot() {}
 
-	public default void setWheelVelocitySetpoint(AngularVelocity velocity) {
-	}
+  public default void setWheelVelocitySetpoint(AngularVelocity velocity) {}
 
-	public default void setWheelVoltage(Voltage volts) {
-	}
+  public default void setWheelVoltage(Voltage volts) {}
 
-	public default void setPivotPosition(Angle position) {
-	}
+  public default void setPivotPosition(Angle position) {}
 
-	public default void setPivotPositionSetpoint(Angle position) {
-	}
+  public default void setPivotPositionSetpoint(Angle position) {}
 
-	public default void setPivotVoltage(Voltage volts) {
-	}
+  public default void setPivotVoltage(Voltage volts) {}
 
-	public default void setPivotVoltageDirect(Voltage volts) {
-	}
+  public default void setPivotVoltageDirect(Voltage volts) {}
 
-	public default void enabledInit() {}
-
+  public default void enabledInit() {}
 }
--- src/main/java/frc/robot/subsystems/Intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeIOSim.java
@@ -7,6 +7,8 @@
 
 package frc.robot.subsystems.Intake;
 
+import static edu.wpi.first.units.Units.*;
+
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
@@ -21,148 +23,129 @@ import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 
-import static edu.wpi.first.units.Units.*;
-
-
 public class IntakeIOSim implements IntakeIO {
-	private final DCMotorSim wheelMotor = configureWheelMotor();
-	private final SingleJointedArmSim pivotMotor = configurePivotMotor();
+  private final DCMotorSim wheelMotor = configureWheelMotor();
+  private final SingleJointedArmSim pivotMotor = configurePivotMotor();
 
-	
+  private static DCMotorSim configureWheelMotor() {
+    DCMotor wheelGearbox = DCMotor.getNEO(1);
+    LinearSystem<N2, N1, N2> wheelPlant =
+        LinearSystemId.createDCMotorSystem(
+            wheelGearbox, IntakeConstants.WHEEL_MOI, IntakeConstants.WHEEL_CONVERSION_FACTOR);
+    return new DCMotorSim(wheelPlant, wheelGearbox);
+  }
 
-	private static DCMotorSim configureWheelMotor() {
-		DCMotor wheelGearbox = DCMotor.getNEO(1);
-		LinearSystem<N2, N1, N2> wheelPlant = LinearSystemId.createDCMotorSystem(
-			wheelGearbox,
-			IntakeConstants.WHEEL_MOI,
-			IntakeConstants.WHEEL_CONVERSION_FACTOR
-		);
-		return new DCMotorSim(wheelPlant, wheelGearbox);
-	}
+  private static SingleJointedArmSim configurePivotMotor() {
+    return new SingleJointedArmSim(
+        DCMotor.getNEO(1),
+        IntakeConstants.PIVOT_CONVERSION_FACTOR,
+        SingleJointedArmSim.estimateMOI(
+            IntakeConstants.LENGTH.in(Meters), IntakeConstants.MASS.in(Kilograms)),
+        IntakeConstants.LENGTH.in(Meters),
+        -0.5,
+        IntakeConstants.STARTING_ANGLE.in(Radians) + 0.5,
+        true,
+        IntakeConstants.STARTING_ANGLE.in(Radians),
+        IntakeConstants.PIVOT_ENCODER_DISTANCE_PER_PULSE,
+        0);
+  }
 
-	private static SingleJointedArmSim configurePivotMotor() {
-		return new SingleJointedArmSim(
-			DCMotor.getNEO(1),
-			IntakeConstants.PIVOT_CONVERSION_FACTOR,
-			SingleJointedArmSim.estimateMOI(IntakeConstants.LENGTH.in(Meters), IntakeConstants.MASS.in(Kilograms)),
-			IntakeConstants.LENGTH.in(Meters),
-			-0.5,
-			IntakeConstants.STARTING_ANGLE.in(Radians)+0.5,
-			true,
-			IntakeConstants.STARTING_ANGLE.in(Radians),
-			IntakeConstants.PIVOT_ENCODER_DISTANCE_PER_PULSE,
-			0
-		);
-	}
+  private final PIDController wheelPID =
+      new PIDController(
+          IntakeConstants.WHEEL_SIM_kP, IntakeConstants.WHEEL_SIM_kI, IntakeConstants.WHEEL_SIM_kD);
 
-	private final PIDController wheelPID =
-		new PIDController(
-			IntakeConstants.WHEEL_SIM_kP,
-			IntakeConstants.WHEEL_SIM_kI,
-			IntakeConstants.WHEEL_SIM_kD
-		);
+  private final SimpleMotorFeedforward wheelFF =
+      new SimpleMotorFeedforward(
+          IntakeConstants.WHEEL_SIM_kS, IntakeConstants.WHEEL_SIM_kV, IntakeConstants.WHEEL_SIM_kA);
 
-	private final SimpleMotorFeedforward wheelFF = new SimpleMotorFeedforward(
-		IntakeConstants.WHEEL_SIM_kS,
-		IntakeConstants.WHEEL_SIM_kV,
-		IntakeConstants.WHEEL_SIM_kA
-	);
+  private final PIDController pivotPID =
+      new PIDController(
+          IntakeConstants.PIVOT_SIM_kP, IntakeConstants.PIVOT_SIM_kI, IntakeConstants.PIVOT_SIM_kD);
 
-	private final PIDController pivotPID = 
-		new PIDController(
-			IntakeConstants.PIVOT_SIM_kP,
-			IntakeConstants.PIVOT_SIM_kI,
-			IntakeConstants.PIVOT_SIM_kD
-		);
+  private final ArmFeedforward pivotFF =
+      new ArmFeedforward(
+          IntakeConstants.PIVOT_SIM_kS,
+          IntakeConstants.PIVOT_SIM_kG,
+          IntakeConstants.PIVOT_SIM_kV,
+          IntakeConstants.PIVOT_SIM_kA);
 
-	private final ArmFeedforward pivotFF =
-		new ArmFeedforward(
-			IntakeConstants.PIVOT_SIM_kS,
-			IntakeConstants.PIVOT_SIM_kG,
-			IntakeConstants.PIVOT_SIM_kV,
-			IntakeConstants.PIVOT_SIM_kA
-		);
+  /** true = controlled by voltage, false = controled by PID + FF */
+  private boolean pivotVoltageMode = false;
 
-	/** true = controlled by voltage, false = controled by PID + FF */
-	private boolean pivotVoltageMode = false;
-	private Voltage pivotAppliedVoltage = Volts.of(0.0);
-	/** true = controlled by voltage, false = controled by PID + FF */
-	private boolean wheelVoltageMode = false;
-	
-	public IntakeIOSim(){
-		this.setPivotPositionSetpoint(IntakeConstants.STARTING_ANGLE);
-	}
+  private Voltage pivotAppliedVoltage = Volts.of(0.0);
+  /** true = controlled by voltage, false = controled by PID + FF */
+  private boolean wheelVoltageMode = false;
 
-	@Override
-	public void updateInputs(IntakeIOInputs inputs) {
-		wheelMotor.update(0.02);
-		pivotMotor.update(0.02);
+  public IntakeIOSim() {
+    this.setPivotPositionSetpoint(IntakeConstants.STARTING_ANGLE);
+  }
 
-		runLoopControl();
+  @Override
+  public void updateInputs(IntakeIOInputs inputs) {
+    wheelMotor.update(0.02);
+    pivotMotor.update(0.02);
 
-		inputs.wheelPosition = wheelMotor.getAngularPosition();
-		inputs.wheelVelocity = wheelMotor.getAngularVelocity();
-		inputs.wheelDesiredVelocity = RadiansPerSecond.of(wheelPID.getSetpoint());
-		inputs.wheelAppliedVolts = Volts.of(wheelMotor.getInputVoltage());
-		inputs.wheelSupplyCurrent = Amps.of(wheelMotor.getCurrentDrawAmps());
+    runLoopControl();
 
-		inputs.pivotPosition = Radians.of(pivotMotor.getAngleRads());
-		inputs.pivotDesiredPosition = Radians.of(pivotPID.getSetpoint());
-		inputs.pivotVelocity = RadiansPerSecond.of(pivotMotor.getVelocityRadPerSec());
-		inputs.pivotAppliedVolts = this.pivotAppliedVoltage;
-		inputs.pivotSupplyCurrent = Amps.of(pivotMotor.getCurrentDrawAmps());
-	}
+    inputs.wheelPosition = wheelMotor.getAngularPosition();
+    inputs.wheelVelocity = wheelMotor.getAngularVelocity();
+    inputs.wheelDesiredVelocity = RadiansPerSecond.of(wheelPID.getSetpoint());
+    inputs.wheelAppliedVolts = Volts.of(wheelMotor.getInputVoltage());
+    inputs.wheelSupplyCurrent = Amps.of(wheelMotor.getCurrentDrawAmps());
 
-	private void runLoopControl() {
-		if (!wheelVoltageMode) {
-			wheelMotor.setInputVoltage(
-				wheelPID.calculate(wheelMotor.getAngularVelocity().in(RadiansPerSecond))
-				+
-				wheelFF.calculate(wheelPID.getSetpoint())
-			);
-		}
+    inputs.pivotPosition = Radians.of(pivotMotor.getAngleRads());
+    inputs.pivotDesiredPosition = Radians.of(pivotPID.getSetpoint());
+    inputs.pivotVelocity = RadiansPerSecond.of(pivotMotor.getVelocityRadPerSec());
+    inputs.pivotAppliedVolts = this.pivotAppliedVoltage;
+    inputs.pivotSupplyCurrent = Amps.of(pivotMotor.getCurrentDrawAmps());
+  }
 
-		if (!pivotVoltageMode) {
-			pivotMotor.setInputVoltage(
-				pivotPID.calculate(pivotMotor.getAngleRads())
-				+
-				pivotFF.calculate(pivotPID.getSetpoint(), 0)
-			);
-		}
-	}
+  private void runLoopControl() {
+    if (!wheelVoltageMode) {
+      wheelMotor.setInputVoltage(
+          wheelPID.calculate(wheelMotor.getAngularVelocity().in(RadiansPerSecond))
+              + wheelFF.calculate(wheelPID.getSetpoint()));
+    }
 
-	@Override
-	public void setWheelVelocitySetpoint(AngularVelocity velocity) {
-		this.wheelVoltageMode = false;
-		wheelPID.setSetpoint(velocity.in(RadiansPerSecond));
-	}
+    if (!pivotVoltageMode) {
+      pivotMotor.setInputVoltage(
+          pivotPID.calculate(pivotMotor.getAngleRads())
+              + pivotFF.calculate(pivotPID.getSetpoint(), 0));
+    }
+  }
 
-	@Override
-	public void setWheelVoltage(Voltage volts) {
-		this.wheelVoltageMode = true;
-		wheelMotor.setInputVoltage(volts.in(Volts));
-	}
+  @Override
+  public void setWheelVelocitySetpoint(AngularVelocity velocity) {
+    this.wheelVoltageMode = false;
+    wheelPID.setSetpoint(velocity.in(RadiansPerSecond));
+  }
 
-	@Override
-	public void stopWheel() {
-		setWheelVoltage(Volts.of(0.0));
-	}
+  @Override
+  public void setWheelVoltage(Voltage volts) {
+    this.wheelVoltageMode = true;
+    wheelMotor.setInputVoltage(volts.in(Volts));
+  }
 
-	@Override
-	public void setPivotPositionSetpoint(Angle position) {
-		this.pivotVoltageMode = false;
-		pivotPID.setSetpoint(position.in(Radians));
-	}
+  @Override
+  public void stopWheel() {
+    setWheelVoltage(Volts.of(0.0));
+  }
 
-	@Override
-	public void setPivotVoltage(Voltage volts) {
-		this.pivotVoltageMode = true;
-		pivotMotor.setInputVoltage(volts.in(Volts));
-		this.pivotAppliedVoltage = volts;
-	}
-	
-	@Override
-	public void stopPivot() {
-		setPivotVoltage(Volts.of(0.0));
-	}
+  @Override
+  public void setPivotPositionSetpoint(Angle position) {
+    this.pivotVoltageMode = false;
+    pivotPID.setSetpoint(position.in(Radians));
+  }
+
+  @Override
+  public void setPivotVoltage(Voltage volts) {
+    this.pivotVoltageMode = true;
+    pivotMotor.setInputVoltage(volts.in(Volts));
+    this.pivotAppliedVoltage = volts;
+  }
+
+  @Override
+  public void stopPivot() {
+    setPivotVoltage(Volts.of(0.0));
+  }
 }
