fc199368017802d1bbe9374a87fcd7fc409f715f
Avi Sharma <64670742+Av1Sharma@users.noreply.github.com>
2026-02-05 11:41:19 -0500

    id cleanup

--- src/main/java/frc/robot/subsystems/flywheel/FlywheelConstants.java
+++ src/main/java/frc/robot/subsystems/flywheel/FlywheelConstants.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems.flywheel;
 
 public class FlywheelConstants {
-  public static final int FLYWHEEL_LEADER_ID = 15; // these ids most likely need to be changed
-  public static final int FLYWHEEL_FOLLOWER_ID = 16;
+  public static final int FLYWHEEL_LEADER_ID = 18;
+  public static final int FLYWHEEL_FOLLOWER_ID = 19;
 }
